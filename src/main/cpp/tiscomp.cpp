daa44891857f36dfade0bcf840af88e65c232f2b
airminer <szepkuti.bence@gmail.com>
2017-11-06 00:45:37 +0000

    Restrict package availability to match original implementation

--- src/main/cpp/tiscomp.cpp
+++ src/main/cpp/tiscomp.cpp
@@ -8,7 +8,94 @@ int main()
 
 	L = luaL_newstate();
 
-	luaL_openlibs(L);
+	//luaL_openlibs(L);
+
+	//Base library
+	luaL_requiref(L, "base", luaopen_base, false);
+
+	lua_pushstring(L, "dofile");
+	lua_pushnil(L);
+	lua_settable(L, -3);
+
+	lua_pushstring(L, "load");
+	lua_pushnil(L);
+	lua_settable(L, -3);
+
+	lua_pushstring(L, "loadfile");
+	lua_pushnil(L);
+	lua_settable(L, -3);
+
+	lua_pushstring(L, "loadstring");
+	lua_pushnil(L);
+	lua_settable(L, -3);
+
+	lua_pushstring(L, "getmetatable");
+	lua_pushnil(L);
+	lua_settable(L, -3);
+
+	lua_pushstring(L, "setmetatable");
+	lua_pushnil(L);
+	lua_settable(L, -3);
+
+	lua_pushstring(L, "rawequal");
+	lua_pushnil(L);
+	lua_settable(L, -3);
+
+	lua_pushstring(L, "rawget");
+	lua_pushnil(L);
+	lua_settable(L, -3);
+
+	lua_pushstring(L, "rawset");
+	lua_pushnil(L);
+	lua_settable(L, -3);
+
+	lua_pushstring(L, "rawlen");
+	lua_pushnil(L);
+	lua_settable(L, -3);
+
+	lua_pushstring(L, "pcall");
+	lua_pushnil(L);
+	lua_settable(L, -3);
+
+	lua_pushstring(L, "xpcall");
+	lua_pushnil(L);
+	lua_settable(L, -3);
+
+	//package.loaded
+	lua_pushstring(L, "package");
+	lua_createtable(L, 0, 1);
+	lua_pushstring(L, "loaded");
+	lua_createtable(L, 0, 4);
+
+	//Other packages
+	lua_pushstring(L, "string");
+	luaL_requiref(L, "string", luaopen_string, true);
+	lua_settable(L, -3);
+
+	lua_pushstring(L, "table");
+	luaL_requiref(L, "table", luaopen_table, true);
+
+	//Moonsharp alias
+	lua_pushstring(L, "pack");
+	lua_pushvalue(L, -1);
+	lua_gettable(L, -3);
+	lua_settable(L, -9);
+
+	lua_settable(L, -3);
+
+
+	lua_pushstring(L, "math");
+	luaL_requiref(L, "math", luaopen_math, true);
+	lua_settable(L, -3);
+
+	lua_pushstring(L, "bit32");
+	luaL_requiref(L, "bit32", luaopen_bit32, true);
+	lua_settable(L, -3);
+
+	lua_settable(L, -3);
+	lua_settable(L, -3);
+
+	lua_settop(L, 0);
 
 	int status = luaL_loadfile(L, "test.lua");
 	if (status) {
