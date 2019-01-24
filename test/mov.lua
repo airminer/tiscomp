92f324dada5bdee2c1ad4c5a01ef78f6e7624e7f
airminer <szepkuti.bence@gmail.com>
2019-01-24 11:23:38 +0100

    Add mov test to repo

---
+++ test/mov.lua
@@ -0,0 +1,16 @@
+
+function get_streams()
+            --1  2  3  4  5  6  7  8  9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 32 33 34 35 36 37 38 39
+	array =  {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39}
+	return {
+		{ STREAM_INPUT, "IN.A", 0, array },
+		{ STREAM_OUTPUT, "OUT.A", 3, array },
+	}
+end
+
+function get_layout()
+    return {TILE_COMPUTE, TILE_COMPUTE, TILE_COMPUTE, TILE_COMPUTE,
+            TILE_COMPUTE, TILE_COMPUTE, TILE_COMPUTE, TILE_COMPUTE,
+            TILE_COMPUTE, TILE_COMPUTE, TILE_COMPUTE, TILE_COMPUTE}
+end
+
